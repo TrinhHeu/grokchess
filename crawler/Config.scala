9caf4d8669c8af90cdee2ea21a949684d9cebc99
TrinhHeu <trinhquochuy2412@gmail.com>
2024-10-27 19:26:51 +0700

    Update Config.scala
    tôi đã sửa

--- crawler/Config.scala
+++ crawler/Config.scala
@@ -5,39 +5,32 @@ import ciris.http4s.*
 import org.http4s.Uri
 
 case class AppConfig(
-    val kafkaServer: String,
-    val downloader: DownloaderConfig
+     kafkaServer: String,
+     downloader: DownloaderConfig
 )
 
 case class DownloaderConfig(
-    val url: Uri,
-    val totalGames: Option[Int],
-    val skipLines: Option[Int]
+     url: Uri,
+     totalGames: Option[Int],
+     skipLines: Option[Int]
 )
 
 object Config:
-
-  def load: IO[AppConfig] =
-    (
-      kafkaServer,
-      downloaderConfig,
-    ).mapN(AppConfig.apply).load[IO]
+def load: IO[AppConfig] =
+    (kafkaServer, downloaderConfig).mapN(AppConfig.apply).load[IO]
 
   def downloaderConfig =
-    (
-      downloadUrl,
-      totalGames,
-      skipLines
-    ).mapN(DownloaderConfig.apply)
+    (downloadUrl, totalGames, skipLines).mapN(DownloaderConfig.apply)
 
   def kafkaServer =
     env("KAFKA_SERVER").as[String].default("localhost:9094")
 
   def downloadUrl =
-    env("DOWNLOAD_URL").as[Uri]
+    env("DOWNLOAD_URL").as[Uri].orThrow
 
   def totalGames =
     env("TOTAL_GAMES").as[Int].option
 
   def skipLines =
     env("SKIP_LINES").as[Int].option
+ 
